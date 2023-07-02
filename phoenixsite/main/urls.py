88d125c87c5a3deb62f912f9b7d12a78fbbd6028
LornaPrecious <pmwendwa085@gmail.com>
2023-07-02 22:07:29 +0300

    Successful completion of adding templates plus static files

--- phoenixsite/main/urls.py
+++ phoenixsite/main/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from . import views 
 
 urlpatterns = [
-    path("", views.index, name = "index"),
+    path("", views.home, name = "home"),
     path("aboutus/", views.aboutus, name = "aboutus"),
     path("contactus/", views.contactus, name = "contactus"),
     path("products/", views.products, name = "products"),
-    path("<int:id>", views.productsid, name = "productsid") #dynamic pages in terms of linking
+    #path("<int:id>", views.productsid, name = "productsid") #dynamic pages in terms of linking
 ]
